3573bed9cf1c6ac1ef3c9e7d042a8b7d27e171b4
weidler <web@tonioweidler.de>
2019-11-01 19:54:16 +0100

    Ported PPO networks for gym environments to functional api.

--- models/fully_connected.py
+++ models/fully_connected.py
@@ -8,100 +8,39 @@ from gym.spaces import Box
 from utilities.util import env_extract_dims
 
 
-class PPOActorFNN(tf.keras.Model):
-    """Fully-connected network taking the role of an actor."""
+def build_ffn_actor_model(env: gym.Env):
+    continuous_control = isinstance(env.action_space, Box)
+    state_dimensionality, n_actions = env_extract_dims(env)
 
-    def __init__(self, env: gym.Env):
-        super().__init__()
+    inputs = tf.keras.Input(shape=(state_dimensionality,))
+    x = tf.keras.layers.Dense(64)(inputs)
+    x = tf.keras.layers.Activation("tanh")(x)
+    x = tf.keras.layers.Dense(64)(x)
+    x = tf.keras.layers.Activation("tanh")(x)
 
-        self.continuous_control = isinstance(env.action_space, Box)
-        self.state_dimensionality, self.n_actions = env_extract_dims(env)
+    if continuous_control:
+        means = tf.keras.layers.Dense(n_actions, input_dim=64)(x)
+        means = tf.keras.layers.Activation("linear")(means)
+        stdevs = tf.keras.layers.Dense(n_actions, input_dim=64)(x)
+        stdevs = tf.keras.layers.Activation("softplus")(stdevs)
 
-        self.fc_a = tf.keras.layers.Dense(64, input_dim=self.state_dimensionality, activation="tanh")
-        self.fc_b = tf.keras.layers.Dense(64, input_dim=64, activation="tanh")
+        out = tf.keras.layers.Concatenate()([means, stdevs])
+    else:
+        x = tf.keras.layers.Dense(n_actions)(x)
+        out = tf.keras.layers.Activation("softmax")(x)
 
-        self.fc_out = tf.keras.layers.Dense(self.n_actions,
-                                            input_dim=64,
-                                            activation="softmax" if not self.continuous_control else "linear")
-
-        if self.continuous_control:
-            self.fc_stdev = tf.keras.layers.Dense(self.n_actions,
-                                                  input_dim=64,
-                                                  activation="softplus")
-
-        # initialize weights though sample input
-        example_input = env.reset().reshape([1, -1]).astype(numpy.float32)
-        self.predict(example_input)
-
-    def call(self, input_tensor, training=False, **kwargs):
-        x = self.fc_a(input_tensor)
-        x = self.fc_b(x)
-
-        out = self.fc_out(x)
-        if self.continuous_control:
-            stdev = self.fc_stdev(x)
-            out = tf.concat((out, stdev), axis=1)
-
-        return out
+    return tf.keras.Model(inputs=inputs, outputs=out)
 
 
-class PPOCriticFNN(tf.keras.Model):
-    """Fully-connected network taking the role of the critic."""
+def build_ffn_critic_model(env: gym.Env):
+    state_dimensionality, n_actions = env_extract_dims(env)
 
-    def __init__(self, env):
-        super().__init__()
+    inputs = tf.keras.Input(shape=(state_dimensionality,))
+    x = tf.keras.layers.Dense(64, input_dim=state_dimensionality)(inputs)
+    x = tf.keras.layers.Activation("tanh")(x)
+    x = tf.keras.layers.Dense(64, input_dim=64)(x)
+    x = tf.keras.layers.Activation("tanh")(x)
+    x = tf.keras.layers.Dense(1, input_dim=64)(x)
+    out = tf.keras.layers.Activation("linear")(x)
 
-        self.state_dimensionality, self.n_actions = env_extract_dims(env)
-
-        # shared base net
-        self.fc_a = tf.keras.layers.Dense(64, input_dim=self.state_dimensionality, activation="tanh")
-        self.fc_b = tf.keras.layers.Dense(64, input_dim=64, activation="tanh")
-        self.fc_out = tf.keras.layers.Dense(1, input_dim=64, activation="linear")
-
-        example_input = env.reset().reshape([1, -1]).astype(numpy.float32)
-        self.predict(example_input)
-
-    def call(self, input_tensor, training=False, **kwargs):
-        x = self.fc_a(input_tensor)
-        x = self.fc_b(x)
-        state_value = self.fc_out(x)
-
-        return state_value
-
-
-class PPOActorCriticNetwork(tf.keras.Model):
-    """Fully-connected network with two heads: Actor and Critic.
-
-    One head produces state-value predictions (critic), the other a probability distribution over actions (actor).
-    This entails that the abstraction part of the network that theoretically is supposed to encode relevant information
-    from the state has shared parameters, as the same information should be useful for both actor and critic. Only the
-    analytical part is specific to the role.
-    """
-
-    def __init__(self, env: gym.Env, continuous: bool):
-        super().__init__()
-
-        self.state_dimensionality, self.n_actions = env_extract_dims(env)
-
-        # shared base net
-        self.fc_a = tf.keras.layers.Dense(16, input_dim=self.state_dimensionality, activation="tanh")
-        self.fc_b = tf.keras.layers.Dense(32, input_dim=16, activation="tanh")
-        self.fc_c = tf.keras.layers.Dense(12, input_dim=32, activation="tanh")
-
-        # role specific heads
-        self.fc_critic_a = tf.keras.layers.Dense(12, input_dim=12, activation="tanh")
-        self.fc_critic_out = tf.keras.layers.Dense(1, input_dim=12, activation="linear")
-
-        self.fc_actor_a = tf.keras.layers.Dense(12, input_dim=12, activation="tanh")
-        self.fc_actor_out = tf.keras.layers.Dense(self.n_actions, input_dim=12,
-                                                  activation="softmax" if not continuous else "linear")
-
-    def call(self, input_tensor, training=False, **kwargs):
-        x = self.fc_a(input_tensor)
-        x = self.fc_b(x)
-        x = self.fc_c(x)
-
-        state_value = self.fc_critic_out(self.fc_critic_a(x))
-        action_probabilities = self.fc_actor_out(self.fc_actor_a(x))
-
-        return action_probabilities, state_value
+    return tf.keras.Model(inputs=inputs, outputs=out)
