Changed Buffering technique for speed up; Fixed advantage calculation in masked buffers; Fixed Memory Leak in Ray Workers;

agent/gather.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 """Functions for gathering experience and communicating it to the main thread."""
+import cProfile
 import os
 from inspect import getfullargspec as fargs
 from typing import Tuple
@@ -9,19 +10,20 @@ import numpy as np
 import ray
 from gym.spaces import Box, Discrete, Dict
 from tqdm import tqdm
+import guppy
 
 import models
 from agent.core import estimate_episode_advantages
 from agent.dataio import tf_serialize_example, make_dataset_and_stats
 from agent.policy import act_discrete, act_continuous
 from environments import *
-from models import build_ffn_distinct_models, build_shadow_brain_v1
+from models import build_ffn_distinct_models, build_shadow_brain_v1, build_rnn_distinct_models
 from utilities.const import STORAGE_DIR
-from utilities.datatypes import ExperienceBuffer, ModelTuple
+from utilities.datatypes import ExperienceBuffer, ModelTuple, TimeSequenceExperienceBuffer
 from utilities.util import parse_state, add_state_dims, is_recurrent_model, flatten, merge_into_batch
 
 
-@ray.remote(num_cpus=1, num_gpus=0)
+@ray.remote(num_cpus=1, num_gpus=0, max_calls=10)
 def collect(model, horizon: int, env_name: str, discount: float, lam: float, subseq_length: int, pid: int):
     """Collect a batch shard of experience for a given number of timesteps."""
 
@@ -49,12 +51,15 @@ def collect(model, horizon: int, env_name: str, discount: float, lam: float, sub
     is_recurrent = is_recurrent_model(joint)
 
     # buffer storing the experience and stats # TODO actually make this bufferable where the size is predefined
-    buffer: ExperienceBuffer
     if is_recurrent:
         assert horizon % subseq_length == 0, "Subsequence length would require cutting of part of the observations."
-        buffer = ExperienceBuffer.new_recurrent(env, horizon // subseq_length, subseq_length)
+        buffer: TimeSequenceExperienceBuffer = TimeSequenceExperienceBuffer.new(env=env,
+                                                                                size=horizon // subseq_length,
+                                                                                seq_len=subseq_length,
+                                                                                is_continuous=is_continuous,
+                                                                                is_multi_feature=is_shadow_brain)
     else:
-        buffer = ExperienceBuffer.new_empty()
+        buffer: ExperienceBuffer = ExperienceBuffer.new_empty(is_continuous, is_shadow_brain)
 
     # go for it
     t, current_episode_return, episode_steps, current_subseq_length = 0, 0, 1, 0
@@ -81,12 +86,13 @@ def collect(model, horizon: int, env_name: str, discount: float, lam: float, sub
 
         # if recurrent, at a subsequence breakpoint or episode end stack the observations and give them to the buffer
         if is_recurrent and (current_subseq_length == subseq_length or done):
+            # TODO next value is inefficient
+            next_value = np.squeeze(joint.predict(add_state_dims(state, dims=2 if is_recurrent else 1))[-1])
             subseq_advantages = estimate_episode_advantages(rewards[-current_subseq_length:],
-                                                            values[-current_subseq_length:] + [0],  # TODO proper last v
+                                                            values[-current_subseq_length:] + [next_value],
                                                             discount, lam)
             subseq_returns = subseq_advantages + values[-current_subseq_length:]
-            buffer.push_seq_to_buffer(states, actions, action_probabilities, subseq_advantages, subseq_returns,
-                                      is_multi_feature=is_shadow_brain, is_continuous=is_continuous)
+            buffer.push_seq_to_buffer(states, actions, action_probabilities, subseq_advantages, subseq_returns)
 
             # reset the buffered information
             states, actions, action_probabilities = [], [], []
@@ -139,8 +145,6 @@ def collect(model, horizon: int, env_name: str, discount: float, lam: float, sub
                     np.array(action_probabilities, dtype=np.float32),
                     advantages,
                     advantages + values[:-1])
-    else:
-        buffer.pad_buffer()
 
     # normalize advantages
     buffer.normalize_advantages()
@@ -154,6 +158,9 @@ def collect(model, horizon: int, env_name: str, discount: float, lam: float, sub
     dataset = dataset.map(tf_serialize_example)
     writer = tfl.data.experimental.TFRecordWriter(f"{STORAGE_DIR}/data_{pid}.tfrecord")
     writer.write(dataset)
+
+    # h = guppy.hpy()
+    # print(h.heap())
 
     return stats
 
@@ -200,18 +207,19 @@ def evaluate(policy_tuple, env_name: str) -> Tuple[int, int]:
 if __name__ == "__main__":
     os.chdir("../")
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-    # tf.config.experimental_run_functions_eagerly(True)
+    os.environ['CUDA_VISIBLE_DEVICES'] = '-1'  # need to deactivate GPU in Debugger mode!
+    tf.config.experimental_run_functions_eagerly(True)
 
-    env_n = "ShadowHand-v1"
-    env = gym.make(env_n)
-    p, v, j = build_shadow_brain_v1(env, 1)
-    joint_tuple = ModelTuple(build_shadow_brain_v1.__name__, j.get_weights())
-    if isinstance(env.observation_space, Dict) and "observation" in env.observation_space.sample():
-        j(merge_into_batch([add_state_dims(env.observation_space.sample()["observation"], dims=1) for _ in range(1)]))
+    # env_n = "ShadowHand-v1"
+    # env = gym.make(env_n)
+    # p, v, j = build_shadow_brain_v1(env, 1)
+    # joint_tuple = ModelTuple(build_shadow_brain_v1.__name__, j.get_weights())
+    # if isinstance(env.observation_space, Dict) and "observation" in env.observation_space.sample():
+    #     j(merge_into_batch([add_state_dims(env.observation_space.sample()["observation"], dims=1) for _ in range(1)]))
 
-    # env_n = "CartPole-v1"
-    # p, v, j = build_ffn_distinct_models(gym.make(env_n))
-    # joint_tuple = ModelTuple(build_ffn_distinct_models.__name__, j.get_weights())
+    env_n = "CartPole-v1"
+    p, v, j = build_rnn_distinct_models(gym.make(env_n))
+    joint_tuple = ModelTuple(build_rnn_distinct_models.__name__, j.get_weights())
 
     ray.init(local_mode=True)
     for i in tqdm(range(10000)):

agent/ppo.py
@@ -178,8 +178,7 @@ class PPOAgent:
         if self.is_recurrent:
             # build and apply a mask over the probabilities (recurrent)
             mask = tf.not_equal(old_action_prob, 0)
-            clipped_safe = tf.where(mask, clipped, tf.zeros_like(clipped))
-            clipped_masked = tf.where(mask, clipped_safe, 0)  # masking with tf.where because inf * 0 = nan...
+            clipped_masked = tf.where(mask, clipped, 0)
             return tf.reduce_sum(clipped_masked) / tf.reduce_sum(tf.cast(mask, tf.float32))
         else:
             return tf.reduce_mean(clipped)
@@ -233,7 +232,7 @@ class PPOAgent:
             return tf.reduce_mean(categorical_entropy_from_log(policy_output))
 
     def drill(self, n: int, epochs: int, batch_size: int, monitor=None, export: bool = False, save_every: int = 0,
-              separate_eval: bool = False) -> "PPOAgent":
+              separate_eval: bool = False, ray_already_initialized: bool = False) -> "PPOAgent":
         """Start a training loop of the agent.
         
         Runs **n** cycles of experience gathering and optimization based on the gathered experience.
@@ -268,7 +267,9 @@ class PPOAgent:
         self.joint.set_weights(weights)
 
         available_cpus = multiprocessing.cpu_count()
-        ray.init(local_mode=self.debug, num_cpus=available_cpus, logging_level=logging.ERROR)
+        if not ray_already_initialized:
+            ray.init(local_mode=self.debug, num_cpus=available_cpus, logging_level=logging.ERROR,
+                     object_store_memory=1e+9, memory=1e+9)
         print(f"Parallelizing {self.workers} Workers Over {available_cpus} Threads.\n")
         for self.iteration in range(self.iteration, n):
             time_dict = OrderedDict()

utilities/datatypes.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
-"""Custom datatypes form simplifying data communication."""
+"""Custom data types for simplifying data communication."""
 import itertools
-import logging
 from collections import namedtuple
 from typing import List, Union
 
 import gym
 import numpy as np
-from keras_preprocessing.sequence import pad_sequences
 from numpy import ndarray as arr
 
 from utilities.util import add_state_dims, env_extract_dims
@@ -22,9 +20,12 @@ class ExperienceBuffer:
 
     def __init__(self, states: Union[List, arr], actions: Union[List, arr], action_probabilities: Union[List, arr],
                  returns: Union[List, arr], advantages: Union[List, arr], episodes_completed: int,
-                 episode_rewards: List[int], episode_lengths: List[int]):
+                 episode_rewards: List[int], episode_lengths: List[int], is_multi_feature: bool, is_continuous: bool):
 
-        self.size = actions.shape[0]
+        self.is_continuous = is_continuous
+        self.is_multi_feature = is_multi_feature
+
+        self.size = actions.shape[0] if actions != [] else 0
         self.filled = 0
 
         self.episode_lengths = episode_lengths
@@ -37,7 +38,7 @@ class ExperienceBuffer:
         self.states = states
 
     def __repr__(self):
-        return f"ExperienceBuffer[{self.filled}/{self.size}]"
+        return f"{self.__class__.__name__}[{self.filled}/{self.size}]"
 
     def fill(self, s, a, ap, ret, adv):
         """Fill the buffer with 5-tuple of experience."""
@@ -46,53 +47,15 @@ class ExperienceBuffer:
         self.size = len(adv)
         self.advantages, self.returns, self.action_probabilities, self.actions, self.states = adv, ret, ap, a, s
 
-    def push_seq_to_buffer(self, states: List[arr], actions: List[arr], action_probabilities: List[arr],
-                           advantages: List[arr], returns: List[arr], is_multi_feature: bool, is_continuous: bool):
-        """Push a sequence to the buffer, constructed from given lists of values."""
-        if is_multi_feature:
-            self.states.append([np.stack(list(map(lambda s: s[f_id], states))) for f_id in range(len(states[0]))])
-        else:
-            self.states.append(np.array(states, dtype=np.float32))
-
-        self.actions.append(np.array(actions, dtype=np.float32 if is_continuous else np.int32))
-        self.action_probabilities.append(np.array(action_probabilities, dtype=np.float32))
-        self.advantages.append(advantages)
-        self.returns.append(returns)
-
-    def pad_buffer(self):
-        """Pad the buffer with zeros to an equal sequence length."""
-        assert np.all([isinstance(f, list) for f in [self.states, self.actions, self.action_probabilities,
-                                                     self.returns,
-                                                     self.advantages]]), "Some part of the experience " \
-                                                                         "is not a list but you want to pad."
-
-        assert np.all([len(f) > 0 for f in [self.states, self.actions, self.action_probabilities,
-                                            self.returns,
-                                            self.advantages]]), "There cannot be an empty s/a/ap/r/adv when padding."
-
-        size_diffs = np.array([len(self.actions), len(self.action_probabilities), len(self.returns),
-                               len(self.advantages)]) == len(self.states)
-        if not np.all(size_diffs):
-            logging.warning(f"Buffer contains inconsistent lengths of s/a/ap/r/adv prior to padding [{size_diffs}].")
-
-        self.states = pad_sequences(self.states, padding="post", dtype=np.float32)
-        self.action_probabilities = pad_sequences(self.action_probabilities, padding="post", dtype=np.float32)
-        self.actions = pad_sequences(self.actions, padding="post")
-        self.returns = pad_sequences(self.returns, padding="post", dtype=np.float32)
-        self.advantages = pad_sequences(self.advantages, padding="post", dtype=np.float32)
-
     def normalize_advantages(self):
         """Normalize the buffered advantages using z-scores. This requires the sequences to be of equal lengths,
         hence if this is not guaranteed during pushing to the buffer, pad_buffer has to be called first."""
-        assert isinstance(self.advantages, arr), "Advantages are still a list of sequences. " \
-                                                 "You should call pad_buffer first."
         rank = len(self.advantages.shape)
         assert rank in [1, 2], f"Illegal rank of advantage tensor, should be 1 or 2 but is {rank}."
 
         mean = np.mean(self.advantages)
         std = np.maximum(np.std(self.advantages), 1e-6)
         self.advantages = (self.advantages - mean) / std
-        # TODO deal with padded
 
     def inject_batch_dimension(self):
         """Add a batch dimension to the buffered experience."""
@@ -103,17 +66,19 @@ class ExperienceBuffer:
         self.advantages = np.expand_dims(self.advantages, axis=0)
 
     @staticmethod
-    def new_empty():
+    def new_empty(is_continuous, is_multi_feature):
         """Return an empty buffer."""
         return ExperienceBuffer(states=[],
                                 actions=[],
                                 action_probabilities=[],
                                 returns=[],
                                 advantages=[],
-                                episodes_completed=0, episode_rewards=[], episode_lengths=[])
+                                episodes_completed=0, episode_rewards=[], episode_lengths=[],
+                                is_continuous=is_continuous,
+                                is_multi_feature=is_multi_feature)
 
     @staticmethod
-    def new(env: gym.Env, size: int):
+    def new(env: gym.Env, size: int, is_continuous, is_multi_feature):
         """Return an empty buffer."""
         state_dim, action_dim = env_extract_dims(env)
 
@@ -126,23 +91,97 @@ class ExperienceBuffer:
                                 action_probabilities=np.zeros((size,)),
                                 returns=np.zeros((size,)),
                                 advantages=np.zeros((size,)),
-                                episodes_completed=0, episode_rewards=[], episode_lengths=[])
+                                episodes_completed=0, episode_rewards=[], episode_lengths=[],
+                                is_continuous=is_continuous,
+                                is_multi_feature=is_multi_feature)
+
+
+class TimeSequenceExperienceBuffer(ExperienceBuffer):
+    """Experience Buffer for TimeSequence Data"""
+
+    def __init__(self, states: Union[List, arr], actions: Union[List, arr], action_probabilities: Union[List, arr],
+                 returns: Union[List, arr], advantages: Union[List, arr], episodes_completed: int,
+                 episode_rewards: List[int], episode_lengths: List[int], is_multi_feature: bool, is_continuous: bool):
+
+        super().__init__(states, actions, action_probabilities, returns, advantages, episodes_completed,
+                         episode_rewards, episode_lengths, is_multi_feature, is_continuous)
+
+        self.true_number_of_transitions = 0
+        self.advantage_mask = np.ones(advantages.shape)
+
+    def push_seq_to_buffer(self, states: List[arr], actions: List[arr], action_probabilities: List[arr],
+                           advantages: List[arr], returns: List[arr]):
+        """Push a sequence to the buffer, constructed from given lists of values."""
+        seq_length = len(actions)
+        self.true_number_of_transitions += seq_length
+
+        if self.is_multi_feature:
+            # can I point out for a second that numpy slicing is beautiful as fu**
+            states = [np.stack(list(map(lambda s: s[f_id], states))) for f_id in range(len(states[0]))]
+        else:
+            states = np.stack(states)
+        advantages = np.stack(advantages)
+
+        self.states[self.filled, :seq_length, ...] = states
+        self.actions[self.filled, :seq_length, ...] = np.stack(actions)
+        self.action_probabilities[self.filled, :seq_length] = action_probabilities
+        self.advantages[self.filled, :seq_length] = advantages
+        self.advantage_mask[self.filled, :seq_length] = np.zeros(advantages.shape)
+        self.returns[self.filled, :seq_length] = returns
+
+        self.filled += 1
+
+    def normalize_advantages(self):
+        """Normalize the buffered advantages using z-scores. This requires the sequences to be of equal lengths,
+        hence if this is not guaranteed during pushing to the buffer, pad_buffer has to be called first."""
+        rank = len(self.advantages.shape)
+        assert rank in [1, 2], f"Illegal rank of advantage tensor, should be 1 or 2 but is {rank}."
+
+        # TODO could better keep track of already masked array in self.advantages?
+        masked_advantages = np.ma.masked_array(self.advantages, self.advantage_mask)
+        mean = masked_advantages.mean()
+        std = np.maximum(masked_advantages.std(), 1e-6)
+        self.advantages = (self.advantages - mean) / std
+
+    # def pad_buffer(self):
+    #     """Pad the buffer with zeros to an equal sequence length."""
+    #     assert np.all([isinstance(f, list) for f in [self.states, self.actions, self.action_probabilities,
+    #                                                  self.returns,
+    #                                                  self.advantages]]), "Some part of the experience " \
+    #                                                                      "is not a list but you want to pad."
+    #
+    #     assert np.all([len(f) > 0 for f in [self.states, self.actions, self.action_probabilities,
+    #                                         self.returns,
+    #                                         self.advantages]]), "There cannot be an empty s/a/ap/r/adv when padding."
+    #
+    #     size_diffs = np.array([len(self.actions), len(self.action_probabilities), len(self.returns),
+    #                            len(self.advantages)]) == len(self.states)
+    #     if not np.all(size_diffs):
+    #         logging.warning(f"Buffer contains inconsistent lengths of s/a/ap/r/adv prior to padding [{size_diffs}].")
+    #
+    #     self.states = pad_sequences(self.states, padding="post", dtype=np.float32)
+    #     self.action_probabilities = pad_sequences(self.action_probabilities, padding="post", dtype=np.float32)
+    #     self.actions = pad_sequences(self.actions, padding="post")
+    #     self.returns = pad_sequences(self.returns, padding="post", dtype=np.float32)
+    #     self.advantages = pad_sequences(self.advantages, padding="post", dtype=np.float32)
 
     @staticmethod
-    def new_recurrent(env: gym.Env, size: int, seq_len):
+    def new(env: gym.Env, size: int, seq_len: int, is_continuous, is_multi_feature):
         """Return an empty buffer for sequences."""
         state_dim, action_dim = env_extract_dims(env)
 
         if isinstance(state_dim, int):
-            state_buffer = np.zeros((size, action_dim))
+            state_buffer = np.zeros((size, seq_len, state_dim), dtype=np.float32)
         else:
             state_buffer = tuple(np.zeros((size, seq_len) + shape) for shape in state_dim)
-        return ExperienceBuffer(states=state_buffer,
-                                actions=np.zeros((size, seq_len, action_dim)),
-                                action_probabilities=np.zeros((size, seq_len,)),
-                                returns=np.zeros((size, seq_len)),
-                                advantages=np.zeros((size, seq_len)),
-                                episodes_completed=0, episode_rewards=[], episode_lengths=[])
+        return TimeSequenceExperienceBuffer(states=state_buffer,
+                                            actions=np.zeros((size, seq_len) + ((action_dim, ) if is_continuous else ()),
+                                                             dtype=np.float32 if is_continuous else np.int32),
+                                            action_probabilities=np.zeros((size, seq_len,), dtype=np.float32),
+                                            returns=np.zeros((size, seq_len), dtype=np.float32),
+                                            advantages=np.zeros((size, seq_len), dtype=np.float32),
+                                            episodes_completed=0, episode_rewards=[], episode_lengths=[],
+                                            is_continuous=is_continuous, is_multi_feature=is_multi_feature)
 
 
 def condense_stats(stat_bundles: List[StatBundle]) -> StatBundle: