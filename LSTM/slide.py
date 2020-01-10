22a9dd804792c6c5bfcc8b38c4be62cfad5e7c16
prstolpe <pr.stolpe@gmail.com>
2020-01-10 11:43:10 +0100

    done

--- LSTM/slide.py
+++ LSTM/slide.py
@@ -1,7 +1,5 @@
-import matplotlib
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.interpolate import interp2d
 from mpl_toolkits.mplot3d import Axes3D
 
 N = 12
@@ -11,13 +9,12 @@ ptss = np.random.uniform(0.4, 0.6, (int(N/3), 3))
 ptsss = np.random.uniform(0.75, 0.9, (int(N/3), 3))
 pts = np.concatenate((pts, ptss, ptsss))
 startpts = np.repeat(np.random.rand(1, 3), N, axis=0)
-#color = np.random.randint(N/2, N*2, 3)
-#color = np.repeat(color, 4)
 color = ('green', 'green', 'green', 'green',
          'red', 'red', 'red', 'red',
          'purple', 'purple', 'purple', 'purple')
 # line = matplotlib.path.Path(, codes=2)
 lines = np.linspace(pts, startpts, 5)
+
 
 def curved_line(point1, point2):
     a = (point2[1] - point1[1])/(np.cosh(point2[0]) - np.cosh(point1[0]))
@@ -25,7 +22,7 @@ def curved_line(point1, point2):
     x = np.linspace(point1[0], point2[0], 10)
     y = a*np.cosh(x) + b
     z = np.linspace(point1[2], point2[2], 10)
-    return (x,y, z)
+    return (x, y, z)
 
 
 fig = plt.figure()
