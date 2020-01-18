ca5e8e865dc6dfeb5ebb6e18c017ee5b4fa51684
prstolpe <pr.stolpe@gmail.com>
2020-01-18 18:44:27 +0100

    Attempt to overcome empty target problem
    Literature has proposes empty target problem. That caused the problem of empty activities. Thus, it is attempted to make target continuously filled with moving average.

--- LSTM/two_d_moving_average.py
+++ LSTM/two_d_moving_average.py
@@ -1,5 +1,7 @@
 import numpy as np
 import tensorflow as tf
+from utilities.model_management import build_sub_model_to
+import matplotlib.pyplot as plt
 
 class Avemover:
 
@@ -17,18 +19,23 @@ class Avemover:
         x = np.random.uniform(-1, 1, nPulses)
         pulses = np.reshape(np.repeat(x, lenP), (nPulses, lenP)).transpose()
 
-        pulse_beginnings = np.random.randint(0, seconds, lenP)
+        pulse_beginnings = np.random.randint(0, seconds, nPulses)
         pulse_beginnings.sort()
+        pulse_beginnings = np.append(pulse_beginnings, seconds)
 
-        input = np.zeros((int(seconds/delta_t), 1, 2))
-        target = np.empty(int(seconds/delta_t))
+        input = np.zeros((int(seconds/delta_t), 1, 1))
+        target = np.zeros(int(seconds/delta_t))
         target[0:targetpulse] = np.zeros(targetpulse)
-        for i in range(pulse_beginnings.shape[0]):
+        k = 0
+        for i in range(pulse_beginnings.shape[0]-1):
+            #k += 1
             input[int(pulse_beginnings[i]/delta_t):(int(pulse_beginnings[i]/delta_t)+lenP), 0, 0] = pulses[:, i]
-            time_difference = pulse_beginnings[i] - pulse_beginnings[i-1]
-            if time_difference <= 30 & time_difference >= 5:
-                input[int(pulse_beginnings[i]/delta_t):(int(pulse_beginnings[i]/delta_t)+lenP), 0, 1] = np.ones(lenP)
-                target[int(pulse_beginnings[i]/delta_t-2*targetpulse):int(pulse_beginnings[i]/delta_t-targetpulse)] = np.repeat(np.mean((pulses[0, (i-1)], pulses[0, (i-2)])), targetpulse)
+            # time_difference = pulse_beginnings[i] - pulse_beginnings[i - 1]
+            target[int(pulse_beginnings[i+1] / delta_t - pulse_beginnings[i] / delta_t):int(pulse_beginnings[i+1] / delta_t)] \
+                = np.mean((pulses[0, i], pulses[0, (i - 1)]))# , int(pulse_beginnings[i+1] / delta_t - pulse_beginnings[i] / delta_t))
+            # if time_difference <= 30 & time_difference >= 5:
+            #input[int(pulse_beginnings[i+1]/delta_t):(int(pulse_beginnings[i+1]/delta_t)+lenP), 0, 1] = np.ones(lenP)
+
 
         return tf.convert_to_tensor(input, dtype=tf.float32), tf.convert_to_tensor(target, dtype=tf.float32)
 
@@ -46,16 +53,34 @@ class Avemover:
 
         return model
 
+
 if __name__ == "__main__":
 
-    mover = Avemover(100, 1000)
-    input, target = mover.gen_data(100)
-    model = mover.build_model((1, 2))
+    mover = Avemover(N=100, seconds=100)
+    input, target = mover.gen_data(nPulses=10)
+    model = mover.build_model((1, 1))
 
     model.compile(optimizer="rmsprop", loss="mse",
                   metrics=['accuracy'])
 
     history = model.fit(input, target, epochs=5)
+
+    prediction = model.predict(input)
+
+    weights = layer = model.get_layer('rnn').get_weights()
+    sub_model = build_sub_model_to(model, ['input', 'rnn'])
+    sub_model.summary()
+
+    activations = sub_model.predict(input)
+    print(activations)
+
+    plt.plot(target.numpy())
+    plt.show()
+    input = input.numpy()
+
+    # plt.plot(input[:, 0, :])
+    # plt.show()
+
 
 
 
