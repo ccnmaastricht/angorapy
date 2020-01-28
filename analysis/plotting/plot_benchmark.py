5f63d905e2fd24671c5732d46102d6dbdd10fd2f
weidler <web@tonioweidler.de>
2020-01-28 18:03:31 +0100

    multi-file benchmarking plots

--- analysis/plotting/plot_benchmark.py
+++ analysis/plotting/plot_benchmark.py
@@ -9,12 +9,14 @@ from utilities.plotting import plot_with_confidence
 colors = ["red", "green", "blue", "orange"]
 
 parser = argparse.ArgumentParser()
-parser.add_argument("exp", nargs="?", type=str, default="BipedalWalker_continuous_continuous_beta", help="name of the experiment")
+parser.add_argument("exp", nargs="*", type=str, default="LunarLanderContinuous_continuous_continuous_beta", help="name of the experiment")
 
 args = parser.parse_args()
 
-with open(f"../../docs/benchmarks/{args.exp}.json") as f:
-    results = json.load(f)
+results = {}
+for exp in args.exp:
+    with open(f"docs/benchmarks/{exp}.json") as f:
+        results.update(json.load(f))
 
 x = list(range(1, len(results[list(results.keys())[0]][0]) + 1))
 
@@ -29,5 +31,5 @@ for i, name in enumerate(results):
 
 plt.legend()
 plt.title(f"Benchmarking {args.exp}")
-plt.savefig(f"../../docs/benchmarks/benchmarking_{args.exp}.pdf", format="pdf")
+plt.savefig(f"docs/benchmarks/benchmarking_{args.exp}.pdf", format="pdf")
 plt.show()
