3c6105fa71798aa539676623c0c4caaf2cbc3c4d
prstolpe <pr.stolpe@gmail.com>
2020-03-20 10:59:29 +0100

    Deleted files to be moved to rnn_dynamical_systems

--- scripts/plot_cc_benchmarks.sh
+++
@@ -1,6 +0,0 @@
-python -m analysis.plotting.plot_benchmark.py default_CartPole-v0 --only discrete_no_norms
-python -m analysis.plotting.plot_benchmark.py default_CartPole-v1 --only discrete_no_norms_short
-python -m analysis.plotting.plot_benchmark.py default_Acrobot-v1 --only discrete
-python -m analysis.plotting.plot_benchmark.py default_MountainCar-v0 --only discrete
-python -m analysis.plotting.plot_benchmark.py default_MountainCarContinuous-v0 --only continuous
-python -m analysis.plotting.plot_benchmark.py workstation_Pendulum-v0 --only pendulum_beta
--- scripts/pullbenchmarks.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-echo -n "username: "
-read USERNM
-
-scp -r "$USERNM"@192.168.26.181:~/dexterous-robot-hand/docs/benchmarks/workstation_* docs/benchmarks/
