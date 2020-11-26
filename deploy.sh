4e7eac9610edc82bc33c2e5a171621a6d9a7b922
Tonio Weidler <admin@tonioweidler.de>
2020-11-26 13:16:45 +0100

    Deploy script

--- deploy.sh
+++ deploy.sh
@@ -10,14 +10,12 @@
 #SBATCH --constraint=gpu
 #SBATCH --time=08:00:00
 
-export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
-export CRAY_CUDA_MPS=1
-
 # load modules
 module load daint-gpu
-module load cray-python
 module load Horovod
-module load cudatoolkit/10.2.89_3.28-7.0.2.1_2.17__g52c0314
+#module load cudatoolkit/10.2.89_3.28-7.0.2.1_2.17__g52c0314
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
 export NCCL_DEBUG=INFO
 export NCCL_IB_HCA=ipogif0
