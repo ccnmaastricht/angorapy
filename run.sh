f2e4ddb7f068c4174d5710a2308242b092535bd3
Tonio Weidler <admin@tonioweidler.de>
2021-04-12 16:57:43 +0200

    Updated run.sh with latest findings about modules to be loaded.

--- run.sh
+++ run.sh
@@ -14,8 +14,9 @@ export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 export CRAY_CUDA_MPS=1
 
 module load daint-gpu
-module load cray-python
 module load cudatoolkit
+module use /apps/daint/UES/6.0.UP04/sandboxes/sarafael/modules/all
+module load cuDNN/8.0.3.33
 
 # load virtual environment
 source ${HOME}/robovenv/bin/activate
