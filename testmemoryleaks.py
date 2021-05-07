a6fb53a38c11439aaa2a17a280eedf98734a8b5d
Tonio Weidler <admin@tonioweidler.de>
2021-05-07 11:04:09 +0200

    Small tests on memory.

--- testmemoryleaks.py
+++ testmemoryleaks.py
@@ -1,7 +1,10 @@
+import os
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
 import time
 
 import tensorflow as tf
-from tqdm import tqdm
 
 from common.const import VISION_WH
 from common.policies import BetaPolicyDistribution
@@ -10,8 +13,8 @@ from models import get_model_builder
 
 # tf.config.run_functions_eagerly(False)
 
-sequence_length = 100
-batch_size = 256
+sequence_length = 8
+batch_size = 128
 
 model_builder = get_model_builder(model="shadow", model_type="gru", shared=False, blind=False)
 
@@ -26,23 +29,34 @@ _, _, model = model_builder(
 optimizer = tf.keras.optimizers.SGD()
 
 sample_batch = {
-    "vision": tf.random.normal([batch_size, sequence_length, VISION_WH, VISION_WH, 3]),
-    "proprioception": tf.random.normal([batch_size, sequence_length, 48]),
-    "somatosensation": tf.random.normal([batch_size, sequence_length, 92]),
-    "goal": tf.random.normal([batch_size, sequence_length, 15])
+    "vision": tf.random.normal([16, sequence_length, VISION_WH, VISION_WH, 3]),
+    "proprioception": tf.random.normal([16, sequence_length, 48]),
+    "somatosensation": tf.random.normal([16, sequence_length, 92]),
+    "goal": tf.random.normal([16, sequence_length, 15])
 }
 
+dataset = tf.data.Dataset.from_tensor_slices(sample_batch)
+dataset = dataset.repeat(2048 * 64).batch(batch_size)
+
+
 @tf.function
+def _get_grads(batch):
+    with tf.GradientTape() as tape:
+        out, v = model(batch, training=True)
+        loss = tf.reduce_mean(out - v)
+
+    grads = tape.gradient(loss, model.trainable_variables)
+
+    return grads
+
+
 def _train():
     start_time = time.time()
 
-    for _ in tqdm(range(sequence_length), disable=False):
-        with tf.GradientTape() as tape:
-            out, v = model(sample_batch, training=True)
-            loss = tf.reduce_mean(out - v)
-
-        grads = tape.gradient(loss, model.trainable_variables)
-        optimizer.apply_gradients(zip(grads, model.trainable_variables))
+    for epoch in range(10):
+        for batch in dataset:
+            grads = _get_grads(batch)
+            optimizer.apply_gradients(zip(grads, model.trainable_variables))
 
     print(f"Execution Time: {time.time() - start_time}")
 
