483121d59368f13f74ec26152e4801239aabea60
Tonio Weidler <admin@tonioweidler.de>
2021-11-18 23:40:14 +0100

    pulls

--- pull-cscs-experiments.sh
+++ pull-cscs-experiments.sh
@@ -1,6 +1,28 @@
+#!/bin/bash
+
+while getopts ":i:" opt; do
+  case $opt in
+    i) ID="$OPTARG"
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    ;;
+  esac
+done
+
 # zip on server and pull
-ssh daint 'zip -r ~/storage.zip /scratch/snx3000/bp000299/dexterous-robot-hand/storage/ -x $SCRATCH/dexterous-robot-hand/storage/experience/*'
-scp daint:~/storage.zip .
+if [ -z ${ID} ]; then
+  echo $ID
+  echo "NO ID"
+  exit
+  ssh daint 'zip -r ~/storage.zip /scratch/snx3000/bp000299/dexterous-robot-hand/storage/ -x $SCRATCH/dexterous-robot-hand/storage/experience/*'
+  scp daint:~/storage.zip .
+else
+  echo $ID
+  echo "GOT ID"
+  exit
+  ssh daint 'zip -r ~/storage.zip /scratch/snx3000/bp000299/dexterous-robot-hand/storage/experiments/$ID /scratch/snx3000/bp000299/dexterous-robot-hand/storage/saved_models/$ID'
+  scp daint:~/storage.zip .
+fi
 
 # unzip locally and move to correct directory
 unzip -n storage.zip -d storage/
