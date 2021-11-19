4b011aa515bf30f09a7176077aa8d8e215fdee59
weidler <web@tonioweidler.de>
2021-11-19 14:57:13 +0100

    Added single id pulling to script.

--- pull-cscs-experiments.sh
+++ pull-cscs-experiments.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 
-while getopts ":i:" opt; do
+REMOVE=false
+
+while getopts "i:d" opt; do
   case $opt in
     i) ID="$OPTARG"
+    ;;
+    d) REMOVE=true
     ;;
     \?) echo "Invalid option -$OPTARG" >&2
     ;;
   esac
 done
 
+if [ REMOVE ]; then
+  echo "Deleting existing storage zip file."
+  ssh daint "rm ~/storage.zip"
+fi
+
 # zip on server and pull
 if [ -z ${ID} ]; then
-  echo $ID
-  echo "NO ID"
-  exit
+  echo "Zipping entire storage."
   ssh daint 'zip -r ~/storage.zip /scratch/snx3000/bp000299/dexterous-robot-hand/storage/ -x $SCRATCH/dexterous-robot-hand/storage/experience/*'
   scp daint:~/storage.zip .
 else
-  echo $ID
-  echo "GOT ID"
-  exit
-  ssh daint 'zip -r ~/storage.zip /scratch/snx3000/bp000299/dexterous-robot-hand/storage/experiments/$ID /scratch/snx3000/bp000299/dexterous-robot-hand/storage/saved_models/$ID'
+  echo "Updating/Adding $ID to zip file."
+  ssh daint "zip -u -r ~/storage.zip /scratch/snx3000/bp000299/dexterous-robot-hand/storage/experiments/$ID/ /scratch/snx3000/bp000299/dexterous-robot-hand/storage/saved_models/$ID/"
   scp daint:~/storage.zip .
 fi
 
