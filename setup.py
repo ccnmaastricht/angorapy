137287351a5280832c0c75a9ac0902756f0ba874
weidler <admin@tonioweidler.de>
2022-09-12 09:37:35 +0200

    [DEPENDENCY] Bumped TF version to 2.10.0.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
         "Box2D",
         "gym==0.24.0",
         "mujoco",
-        "tensorflow==2.9.1",
+        "tensorflow==2.10.0",
         "mpi4py==3.1.3",
         "tqdm",
         "simplejson",
