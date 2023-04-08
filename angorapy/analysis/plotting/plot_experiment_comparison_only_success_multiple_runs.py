5e5ea8bd4c3571d193b9ffdf3d018b993ae7a330
weidler <admin@tonioweidler.de>
2023-04-08 23:37:34 +0200

    .

--- angorapy/analysis/plotting/plot_experiment_comparison_only_success_multiple_runs.py
+++ angorapy/analysis/plotting/plot_experiment_comparison_only_success_multiple_runs.py
@@ -8,6 +8,7 @@ import pandas as pd
 import seaborn as sns
 from matplotlib import pyplot as plt
 import matplotlib
+import statsmodels.stats.api as sms
 
 from angorapy.common.const import PATH_TO_EXPERIMENTS, QUALITATIVE_COLOR_PALETTE
 
@@ -23,6 +24,8 @@ matplotlib.rc('font', **font)
 #                   ['1674343261520188', '1674308148646663', '1674074113967956', '1674074113731734', '1673350499432390']]  # compare distributions
 experiment_ids = [['1680268307741971', '1680267170902356', '1680266899101867', '1679765936722940', '1679142835973298'],
                   ['1674343261520188', '1674308148646663', '1674074113967956', '1674074113731734', '1673350499432390']]  # compare distributions
+# experiment_ids = [['1680268307741971', '1680267170902356', '1679765936722940', '1679142835973298'],
+#                   ['1674343261520188', '1674308148646663', '1674074113967956', '1673350499432390']]  # compare distributions
 names = ["FPN model", "OpenAI"]
 
 reward_developments = {}
@@ -39,7 +42,7 @@ for i, group in enumerate(experiment_ids):
     reward_developments[exp_name] = []
     cosucc_developments[exp_name] = []
     # evaluation_rewards[exp_name] = []
-    evaluation_reward_histograms[exp_name] = []
+    # evaluation_reward_histograms[exp_name] = []
 
     for id in group:
         with open(os.path.join("../../../", PATH_TO_EXPERIMENTS, str(id), "meta.json")) as f:
@@ -64,12 +67,14 @@ for i, group in enumerate(experiment_ids):
     cosucc_developments[exp_name] = np.array(
         [trace[:min(map(len, cosucc_developments[exp_name]))] for trace in cosucc_developments[exp_name]])
 
+    descm = sms.DescrStatsW(cosucc_developments[exp_name])
+
     rd_mean = np.mean(reward_developments[exp_name], axis=0)
     cd_mean = np.mean(cosucc_developments[exp_name], axis=0)
     rd_std = np.std(reward_developments[exp_name], axis=0)
-    cd_std = np.std(cosucc_developments[exp_name], axis=0)
+    cd_std = descm.tconfint_mean()
     reward_bands[exp_name] = (rd_mean - rd_std, rd_mean + rd_std)
-    cosucc_bands[exp_name] = (cd_mean - cd_std, cd_mean + cd_std)
+    cosucc_bands[exp_name] = (cd_mean - cd_std[0], cd_mean + cd_std[1])
 
 ax1 = plt.subplot2grid((1, 3), (0, 0), colspan=3)
 # ax2 = plt.subplot2grid((1, 3), (0, 2), colspan=1)
@@ -107,7 +112,7 @@ ax1.set_ylabel("Consecutive Goals Reached")
 ax1.legend(loc='upper left',  ncol=1, handlelength=.7, fontsize=18)
 
 ax1.set_xlim(0, x_max)
-ax1.set_ylim(0)
+ax1.set_ylim(0, 50)
 
 plt.gcf().set_size_inches(8, 4)
 plt.subplots_adjust(wspace=0.3, right=0.995, left=0.1, top=0.9, bottom=0.2)
