38d5defaccd8ed6649827bf9dcbefaf0c816c9df
weidler <web@tonioweidler.de>
2023-05-22 12:32:12 +0200

    Expanded Model Tests.

--- tests/test_builtin_models.py
+++ tests/test_builtin_models.py
@@ -14,7 +14,7 @@ def perform_test_on_model(model_name):
     discrete_env = make_env("LunarLander-v2")
     discrete_distr = CategoricalPolicyDistribution(discrete_env)
 
-    multi_discrete_env = make_env("ManipulateBlockDiscreteRelative-v0")
+    multi_discrete_env = make_env("HumanoidManipulateBlockDiscrete-v0")
     multi_discrete_distr = MultiCategoricalPolicyDistribution(multi_discrete_env)
 
     build_model = get_model_builder(model=model_name, model_type="ffn", shared=False)
@@ -23,14 +23,15 @@ def perform_test_on_model(model_name):
     build_shared_recurrent_model = get_model_builder(model=model_name, model_type="lstm", shared=True)
 
     for env, distr in [(cont_env, cont_distr), (discrete_env, discrete_distr), (multi_discrete_env, multi_discrete_distr)]:
-        _, _, joint = build_model(env, distr, bs=1, sequence_length=1)
-        state = env.reset()[0]
-        prepared_state = state.with_leading_dims(time=is_recurrent_model(joint)).dict_as_tf()
-        policy_out = flatten(joint(prepared_state, training=False))
+        for model in [build_model, build_shared_model, build_recurrent_model, build_shared_recurrent_model]:
+            _, _, joint = model(env, distr, bs=1, sequence_length=1)
+            state = env.reset()[0]
+            prepared_state = state.with_leading_dims(time=is_recurrent_model(joint)).dict_as_tf()
+            policy_out = flatten(joint(prepared_state, training=False))
 
-        predicted_distribution_parameters, value = policy_out[:-1], policy_out[-1]
-        # from the action distribution sample an action and remember both the action and its probability
-        action, action_probability = distr.act(*predicted_distribution_parameters)
+            predicted_distribution_parameters, value = policy_out[:-1], policy_out[-1]
+            # from the action distribution sample an action and remember both the action and its probability
+            action, action_probability = distr.act(*predicted_distribution_parameters)
 
 
 class ModelTest(unittest.TestCase):
