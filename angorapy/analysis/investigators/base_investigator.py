c40fa1dae03f6ebb5a659e528b37bc122bd1a0ee
weidler <web@tonioweidler.de>
2023-10-16 13:37:15 +0200

    Add strategies for dealing with recurrent layers in get_layer_activations

--- angorapy/analysis/investigators/base_investigator.py
+++ angorapy/analysis/investigators/base_investigator.py
@@ -133,7 +133,7 @@ class Investigator:
         else:
             raise ValueError("Recurrent layer type not understood. Is it custom?")
 
-    def get_layer_activations(self, layer_names: List[str], input_tensor=None):
+    def get_layer_activations(self, layer_names: List[str], input_tensor=None, recurrent_strategy="first"):
         """Get activations of a layer. If no input tensor is given, a random tensor is used."""
         activations = {}
 
@@ -149,7 +149,10 @@ class Investigator:
 
         for key in activations.keys():
             if isinstance(activations[key], list):
-                activations[key] = activations[key][0]
+                if recurrent_strategy == "first":
+                    activations[key] = activations[key][0]
+                elif recurrent_strategy == "all":
+                    activations[key] = tf.concat(activations[key][1:], axis=-1)
 
         # release the hook to prevent infinite nesting
         clear_hooks(self.network)
